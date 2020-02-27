53821e95e55b0eed9cc38fca1e4a08151437a2eb
Dominik Engel <dominik.engel@mail.de>
2020-02-27 15:57:19 +0100

    fix copy bibtex button

--- js/customscripts.js
+++ js/customscripts.js
@@ -20,6 +20,13 @@ $( document ).ready(function() {
      */
     anchors.add('h2,h3,h4,h5');
 
+    $('.js-tooltip').tooltip();
+    $('.js-copy').click(function () {
+        console.log($(this).contents())
+        var text = $(this).attr('data-copy');
+        var el = $(this);
+        copyToClipboard(text, el);
+    });
 });
 
 // needed for nav tabs on pages. See Formatting > Nav tabs for more details.
@@ -52,3 +59,27 @@ $(function() {
         }
     });
 });
+
+function copyToClipboard(text, el) {
+    var copyTest = document.queryCommandSupported('copy');
+    var elOriginalText = el.attr('data-original-title');
+
+    if (copyTest === true) {
+        var copyTextArea = document.createElement("textarea");
+        copyTextArea.value = text;
+        document.body.appendChild(copyTextArea);
+        copyTextArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+            el.attr('data-original-title', msg).tooltip('show');
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+        document.body.removeChild(copyTextArea);
+        el.attr('data-original-title', elOriginalText);
+    } else {
+        // Fallback if browser doesn't support .execCommand('copy')
+        window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+    }
+}
